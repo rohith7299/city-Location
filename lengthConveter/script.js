aa628b44b76a35a952dcd6f0d3bff0f2a10ff535
rohith <136072025+rohith7299@users.noreply.github.com>
2024-04-24 21:54:24 +0530

    etf

--- lengthConveter/script.js
+++ lengthConveter/script.js
@@ -1,7 +1,35 @@
+const btn = document.querySelector('button')
+btn.addEventListener('click',displaystatus);
+function displaystatus(){
+    const input = document.getElementById('input')
+    const city = input.options[input.selectedIndex].value
+    let population = 0,literacyRate =0,language =''
+    switch(city){
+        case 'Bengaluru':population = 8443675
+                         literacyRate = 88.71
+                         language = 'Kannada'
+                         break
+        case 'Chennai':  population = 4646732
+                         literacyRate = 90.20
+                         language = 'Tamil'
+                         break
 
-function convert(){
-    let cmval = Number( document.getElementById("input").value)
-    let incVal = cmval/2.54
-    let result = document.getElementById("result")
-    result.innerHTML = incVal .toFixed(2) + "inches"
+        case 'Mumbai':
+                      population = 12442373
+                      literacyRate = 89.73
+                      language = 'Marathi'
+                      break
+
+        case 'Delhi':
+                      population = 16787941
+                      literacyRate = 86.20
+                      language = 'Hindi'
+
+                      break
+
+    }
+    let text = `The Indian city of ${city} has a population of ${population}. Language spoken is ${language} and literacy rate is ${literacyRate}%.` 
+    
+    let result =document.getElementById('result')
+    result.innerHTML = text
 }
